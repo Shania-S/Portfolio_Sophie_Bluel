612fcd6c98c2abb02b5f9d062b7080bcacf0e62b
Shania Sidimana <shaniasidimana1@gmail.com>
2023-11-12 16:39:10 +0100

    Added a get request for a test

--- Backend/app.js
+++ Backend/app.js
@@ -20,6 +20,11 @@ const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
 db.sequelize.sync().then(()=> console.log('db is ready'));
+
+app.get("/", (req, res) => { 
+  res.send("Hello"); 
+}); 
+
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);

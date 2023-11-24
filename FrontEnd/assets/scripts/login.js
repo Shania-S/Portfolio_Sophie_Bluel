2242f1f031e49ec3bb528868614b63ef7f1ebaa4
Shania Sidimana <shaniasidimana1@gmail.com>
2023-11-24 21:59:50 +0100

    Add function that stores the token

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("form");
 const errorMsg = document.querySelector(".errorMessage");
 
+/* Vérifie l'identifiant de l'utilisateur, si les info sont fausses
+   on affiche un message d'erreur */
 form.addEventListener("submit", (event) => {
   event.preventDefault();
  
@@ -20,25 +22,22 @@ form.addEventListener("submit", (event) => {
     errorMsg.style.display = "block";
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
-
   return response.json();
-
 }).then((result) => {
-  
   handleResult(result);
-
 }) .catch(error => {
     console.error('Error during POST request:', error.message);
     throw error; 
   });
-
 });
 
-
+/* Dès que l'utilisateur commence à écrire, cette fonction enlève le message d'erreur */
 function removeError() {
   errorMsg.style.display = "none";
 }
 
+/* Si les info sont correctes, on récupère le token 
+   On retourne à la page d'accueil */
 function handleResult(result) {
    window.localStorage.setItem("Token", result.token);
    window.location.href = "./index.html";
